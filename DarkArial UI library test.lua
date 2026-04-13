d25967c67ce6224cdbc31783f9f667c3db23c421
evilrobloxian <ivasuraivan11@gmail.com>
2026-04-13 15:00:54 +0300

    Update DarkArial UI library test.lua

--- DarkArial UI library test.lua
+++ DarkArial UI library test.lua
@@ -105,7 +105,6 @@ function UI:CreateWindow(cfg)
 	TabsFrame.BorderSizePixel = 0
 	Instance.new("UICorner", TabsFrame).CornerRadius = UDim.new(0,5)
 
-	-- OPEN BUTTON IMAGE
 	local Open = Instance.new("ImageButton", gui)
 	Open.Size = UDim2.new(0,52,0,42)
 	Open.Position = UDim2.new(0,32,0,16)
@@ -198,32 +197,36 @@ function UI:CreateWindow(cfg)
 		end
 
 		function Tab:AddBox(cfg)
-			local box = Instance.new("TextBox", Page)
-			box.Size = UDim2.new(0,398,0,40)
-			box.Position = UDim2.new(0.5, -199, 0, offsetY)
+	local box = Instance.new("TextBox", Page)
+	box.Size = UDim2.new(0,398,0,40)
+	box.Position = UDim2.new(0.5, -199, 0, offsetY)
 
-			box.BackgroundColor3 = Color3.fromRGB(150,0,0)
-			box.TextColor3 = Color3.fromRGB(255,255,255)
-			box.Font = Enum.Font.SourceSansBold
-			box.TextSize = 18
+	box.BackgroundColor3 = Color3.fromRGB(150,0,0)
+	box.TextColor3 = Color3.fromRGB(255,255,255)
+	box.Font = Enum.Font.SourceSansBold
+	box.TextSize = 18
 
-			box.Text = ""
-			box.PlaceholderText = "Enter here..."
+	box.Text = ""
+	box.PlaceholderText = "Enter here..."
 
-			box.TextWrapped = true
-			box.TextXAlignment = Enum.TextXAlignment.Left
-			box.TextYAlignment = Enum.TextYAlignment.Center
+	box.TextWrapped = true
+	box.TextXAlignment = Enum.TextXAlignment.Left
+	box.TextYAlignment = Enum.TextYAlignment.Center
 
-			box.BorderSizePixel = 0
+	box.BorderSizePixel = 0
 
-			local pad2 = Instance.new("UIPadding", box)
-			pad2.PaddingLeft = UDim.new(0,6)
+	
+	local corner = Instance.new("UICorner", box)
+	corner.CornerRadius = UDim.new(0,5)
 
-			offsetY += 46
-			updateCanvas(Page)
+	local pad2 = Instance.new("UIPadding", box)
+	pad2.PaddingLeft = UDim.new(0,6)
 
-			return box
-		end
+	offsetY += 46
+	updateCanvas(Page)
+
+	return box
+end
 
 		function Tab:AddToggle(cfg)
 			local state = false
@@ -275,7 +278,7 @@ function UI:CreateWindow(cfg)
 
 			local fill = Instance.new("Frame", bar)
 			fill.Size = UDim2.new(0,0,1,0)
-			fill.BackgroundColor3 = Color3.fromRGB(150,0,0)
+			fill.BackgroundColor3 = Color3.fromRGB(150,0,0) -- CHANGE
 			Instance.new("UICorner", fill)
 
 			offsetY += 56
